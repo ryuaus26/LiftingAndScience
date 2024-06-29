843673175f88f59fe41175e001c20b1635399b79
ryuaus26 <yunryu1217@gmail.com>
2024-06-29 21:25:43 +0900

    First Commit

---
+++ node_modules/@babylonjs/core/Materials/shaderLanguage.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Language of the shader code
+ */
+export declare const enum ShaderLanguage {
+    /** language is GLSL (used by WebGL) */
+    GLSL = 0,
+    /** language is WGSL (used by WebGPU) */
+    WGSL = 1
+}
